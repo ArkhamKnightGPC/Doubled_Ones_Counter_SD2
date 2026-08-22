8b1e0e383501119f443d11694d4c204c26618c60
ArkhamKnightGPC <nazter57@usp.br>
2026-08-22 00:29:52 -0300

    .vcd filename

--- sim/sim_onescounter.sh
+++ sim/sim_onescounter.sh
@@ -11,4 +11,4 @@ ghdl -a --std=08 "${SRC}/onescounter_fd.vhd"
 ghdl -a --std=08 "${SRC}/onescounter.vhd"
 ghdl -a --std=08 "${TB}/onescounter_tb.vhd"
 ghdl -e --std=08 onescounter_tb
-ghdl -r --std=08 onescounter_tb --vcd=onescounter_original.vcd
+ghdl -r --std=08 onescounter_tb --vcd=onescounter.vcd
